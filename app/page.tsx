7adea264adaec7c133c121d8432efd9542181df6
lolieatapple <lolieatapple@gmail.com>
2023-09-12 18:12:28 +0800

    update github url

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 import {MatrixEffect} from './MatrixEffect';
 import { useRouter } from 'next/navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 export default function Home() {
   const router = useRouter();
@@ -9,7 +12,12 @@ export default function Home() {
       <MatrixEffect />
 
       <div className="absolute z-20 flex flex-col items-center justify-center space-y-4">
-        <h1 className="text-4xl font-bold text-green-500">Flash Terminal</h1>
+        <div className="flex items-center space-x-4">
+          <h1 className="text-4xl font-bold text-green-500">Flash Terminal</h1>
+          <a href="https://github.com/lolieatapple/flash-terminal" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} className="text-green-500 text-3xl hover:text-green-600 cursor-pointer" />
+          </a>
+        </div>
         <br />
         <button className="bg-green-500 hover:bg-green-600 px-12 py-2 text-xl text-white font-bold rounded-full transform transition hover:scale-105"
           onClick={()=>router.push('/terminal')}
