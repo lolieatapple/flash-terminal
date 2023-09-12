3fe6ebeafde186eaa76e7adb9fe41d1a8a949548
lolieatapple <lolieatapple@gmail.com>
2023-09-12 11:46:47 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         <button className="bg-green-500 hover:bg-green-600 px-12 py-2 text-xl text-white font-bold rounded-full transform transition hover:scale-105">
           Enter
         </button>
+        <p className="text-green-500 text-sm">* One time terminal of 4-core CPU, 1GB RAM, 3GB HDD machine.</p>
       </div>
     </div>
   )
